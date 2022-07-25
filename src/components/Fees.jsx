fd6879d32afdd43289911cf00d4ea2af2f1a7a99
aleksandrsokolov <timac11@bk.ru>
2022-07-25 17:16:10 +0300

    close https://github.com/dccnconf/web22/issues/13

--- src/components/Fees.jsx
+++ src/components/Fees.jsx
@@ -10,10 +10,20 @@ const Fees = ({ fees, className = "" }) => {
         Registration fees are waived for invited keynote speakers. Please note, that once the registration payment is
         done, <span className="text-red-800 font-bold">no refund is available</span>.
       </p>
-      <p className="md:leading-7 md:text-xl text-gray-700 mt-8">
-        Payment details will be available later.
-      </p>
       <FeesTable fees={fees} className="my-12" />
+      <h3 className="h3 mt-12">Payment information</h3>
+      <p className="md:leading-7 md:text-xl text-gray-700 mt-8">
+        To pay the registration fee (online), please follow the  <a href="https://portal.pfur.ru:4443/Free/OfferPayEn.aspx?num=3222264001150" target="_blank" className="text-blue-500 hover:underline">link</a>.
+        To participate in DCCN-2022 one has to accept the public offer. The public offer is available at the <a href="/downloads/public_offer.pdf" target="_blank" className="text-blue-500 hover:underline">following link</a>.
+      </p>
+      <p className="md:leading-7 md:text-xl text-gray-700 mt-8">
+        To participate in DCCN-2022 one has to accept the public offer. The public offer is available at the following link.
+        Acceptance of the Offer is carried out by paying the registration fee in the order covered by Section 3 of the Offer, and sending a confirmation of payment to the following email
+        address: <a href="mailto:org@dccn.ru" className="text-blue-500 hover:underline">org@dccn.ru</a>.
+      </p>
+      <p className="md:leading-7 md:text-xl text-gray-700 mt-8">
+        <span className="text-red-800 font-bold">Important:</span> when paying, please, completely indicate the first name and the last name of the payer. Otherwise we may not be able to identify the payer.
+      </p>
     </div>
   )
 };
